5443591c9274ccfad07e101d6dca90647e153850
Arpit <34962199+arpitchalakh@users.noreply.github.com>
2026-08-23 14:32:14 +0530

    Add Next.js config

---
+++ web/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '8mb'
+    }
+  }
+};
+
+export default nextConfig;
